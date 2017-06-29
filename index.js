acb5adcd41b13b4c9cab7036c5f24df176b385b9
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2017-06-28 17:28:44 -0700

    convert package to use JavaScript

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-HideItems = require "./lib/hide_items.coffee"
+"use babel";
 
-module.exports =
-  activate: (state) ->
-    @hideItems = new HideItems(state.itemsPaths, state.parents)
-    atom.commands.add "atom-workspace", "hide-files:hide-file",
-                               @hideItems.hideItemCommand.bind @hideItems
-    atom.commands.add "atom-workspace", "hide-files:hide-directory",
-                               @hideItems.hideItemCommand.bind @hideItems
-    atom.commands.add "atom-workspace", "hide-files:unhide-project-files",
-                               @hideItems.unhideItems.bind @hideItems
-    atom.commands.add "atom-workspace", "hide-files:unhide-children",
-                               @hideItems.unHideChildren.bind @hideItems
+let HideItems = require("./lib/hide_items.js");
 
-  serialize: () ->
-    @hideItems.serialize()
+module.exports = {
+  activate(state) {
+    this.hideItems = new HideItems(state.itemsPaths, state.parents);
+    atom.commands.add("atom-workspace", "hide-files:hide-file",
+                               this.hideItems.hideItemCommand.bind(this.hideItems));
+    atom.commands.add("atom-workspace", "hide-files:hide-directory",
+                               this.hideItems.hideItemCommand.bind(this.hideItems));
+    atom.commands.add("atom-workspace", "hide-files:unhide-project-files",
+                               this.hideItems.unhideItems.bind(this.hideItems));
+    atom.commands.add("atom-workspace", "hide-files:unhide-children",
+                               this.hideItems.unHideChildren.bind(this.hideItems));
+  },
+
+  serialize() {
+    this.hideItems.serialize();
+  }
+};
